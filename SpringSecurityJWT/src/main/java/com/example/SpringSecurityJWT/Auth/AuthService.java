2d0e1ae071bf33487f7f00c3afd408804b8b82a0
francosantzz <129998263+francosantzz@users.noreply.github.com>
2023-11-12 20:50:51 -0300

    Subo trabajo

---
+++ SpringSecurityJWT/src/main/java/com/example/SpringSecurityJWT/Auth/AuthService.java
@@ -0,0 +1,46 @@
+package com.example.SpringSecurityJWT.Auth;
+
+import com.example.SpringSecurityJWT.Jwt.JwtService;
+import com.example.SpringSecurityJWT.User.Role;
+import com.example.SpringSecurityJWT.User.User;
+import com.example.SpringSecurityJWT.User.UserRepository;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+
+public class AuthService {
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    public AuthResponse login(LoginRequest request) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
+        UserDetails user=userRepository.findByUsername(request.getUsername()).orElseThrow();
+        String token=jwtService.getToken(user);
+        return AuthResponse.builder()
+                .token(token)
+                .build();
+    }
+
+    public AuthResponse register(RegisterRequest request) {
+        User user = User.builder()
+                .username(request.getUsername())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .country(request.getCountry())
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        return AuthResponse.builder()
+                .token(jwtService.getToken(user))
+                .build();
+    }
+}
---
+++ SpringSecurityJWT/src/main/java/com/example/SpringSecurityJWT/User/Role.java
@@ -0,0 +1,6 @@
+package com.example.SpringSecurityJWT.User;
+
+public enum Role {
+    ADMIN,
+    USER
+}
